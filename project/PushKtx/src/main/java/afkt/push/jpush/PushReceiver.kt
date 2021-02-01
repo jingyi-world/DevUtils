2edfbaf6ed1def23e368e7a54b3606f08fb27f40
Cjt <jtongttt@gmail.com>
2021-02-02 00:15:39 +0800

    1.更新部分代码

--- project/PushKtx/src/main/java/afkt/push/jpush/PushReceiver.kt
+++ project/PushKtx/src/main/java/afkt/push/jpush/PushReceiver.kt
@@ -1,9 +1,14 @@
 package afkt.push.jpush
 
+import afkt.push.router.PushRouterActivity
 import android.content.Context
+import android.content.Intent
+import cn.jpush.android.api.CmdMessage
 import cn.jpush.android.api.CustomMessage
 import cn.jpush.android.api.NotificationMessage
 import cn.jpush.android.service.JPushMessageReceiver
+import dev.utils.app.logger.DevLogger
+
 
 /**
  * detail: 极光推送广播
@@ -15,16 +20,77 @@ class PushReceiver : JPushMessageReceiver() {
     private val TAG = PushReceiver::class.java.simpleName
 
     override fun onMessage(
-        p0: Context?,
-        p1: CustomMessage?
+        context: Context?,
+        customMessage: CustomMessage?
     ) {
-        super.onMessage(p0, p1)
+        DevLogger.dTag(TAG, "[onMessage] ${customMessage}")
+//        // 自定义消息 ( 透传 )
+//        customMessage?.let {
+//            var message = it.message
+//        }
     }
 
     override fun onNotifyMessageOpened(
-        p0: Context?,
-        p1: NotificationMessage?
+        context: Context?,
+        message: NotificationMessage?
     ) {
-        super.onNotifyMessageOpened(p0, p1)
+        DevLogger.dTag(TAG, "[onNotifyMessageOpened] ${message}")
+        // 点击通知消息
+        message?.let {
+            var pushMessage = toPushMessage(message)
+            // 跳转进行推送消息解析分发
+            PushRouterActivity.start(context, pushMessage)
+        }
+    }
+
+    override fun onNotifyMessageArrived(
+        context: Context?,
+        message: NotificationMessage?
+    ) {
+        DevLogger.dTag(TAG, "[onNotifyMessageArrived] ${message}")
+        // 收到通知消息
+    }
+
+    override fun onNotifyMessageDismiss(
+        context: Context?,
+        message: NotificationMessage?
+    ) {
+        DevLogger.dTag(TAG, "[onNotifyMessageDismiss] ${message}")
+    }
+
+    override fun onMultiActionClicked(
+        context: Context?,
+        intent: Intent?
+    ) {
+        DevLogger.dTag(TAG, "[onMultiActionClicked] 用户点击了通知栏按钮")
+    }
+
+    override fun onRegister(
+        context: Context?,
+        registrationId: String?
+    ) {
+        DevLogger.dTag(TAG, "[onRegister] ${registrationId}")
+    }
+
+    override fun onConnected(
+        context: Context?,
+        isConnected: Boolean
+    ) {
+        DevLogger.dTag(TAG, "[onConnected] ${isConnected}")
+    }
+
+    override fun onCommandResult(
+        context: Context?,
+        cmdMessage: CmdMessage?
+    ) {
+        DevLogger.dTag(TAG, "[onCommandResult] ${cmdMessage}")
+    }
+
+    override fun onNotificationSettingsCheck(
+        context: Context?,
+        isOn: Boolean,
+        source: Int
+    ) {
+        DevLogger.dTag(TAG, "[onNotificationSettingsCheck] isOn : ${isOn}, source: ${source}")
     }
 }
--- project/PushKtx/src/main/java/afkt/push/ui/activity/MessageActivity.kt
+++ project/PushKtx/src/main/java/afkt/push/ui/activity/MessageActivity.kt
@@ -2,14 +2,16 @@ package afkt.push.ui.activity
 
 import afkt.push.R
 import afkt.push.base.BaseActivity
-import androidx.viewbinding.ViewBinding
+import afkt.push.databinding.ActivityMessageBinding
+import afkt.push.jpush.PushMessage
+import dev.utils.DevFinal
 
 /**
  * detail: 推送消息 Activity
  * @author Ttt
  * 点击推送后根据, 推送消息跳转对应的路由页
  */
-class MessageActivity : BaseActivity<ViewBinding>() {
+class MessageActivity : BaseActivity<ActivityMessageBinding>() {
 
     override fun isToolBar(): Boolean = true
 
@@ -20,5 +22,22 @@ class MessageActivity : BaseActivity<ViewBinding>() {
     override fun initValue() {
         super.initValue()
         setTitle(TAG) // MessageActivity
+
+        intent?.getParcelableExtra<PushMessage>(DevFinal.DATA)?.run {
+            var builder = StringBuilder()
+                .append("msgId: ${msgId}")
+                .append("${DevFinal.NEW_LINE_STR}")
+                .append("notificationId: ${notificationId}")
+                .append("${DevFinal.NEW_LINE_STR}")
+                .append("notificationTitle: ${notificationTitle}")
+                .append("${DevFinal.NEW_LINE_STR}")
+                .append("notificationContent: ${notificationContent}")
+                .append("${DevFinal.NEW_LINE_STR}")
+                .append("notificationExtras: ${notificationExtras}")
+                .append("${DevFinal.NEW_LINE_STR}")
+                .append("pushExtras routerUri: ${pushExtras?.routerUri}")
+
+            binding.vidAmMessageTv.text = builder.toString()
+        }
     }
 }
