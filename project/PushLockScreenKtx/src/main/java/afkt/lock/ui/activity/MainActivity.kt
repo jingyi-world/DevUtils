1cb89c96614f104f15a7398c3e9f01a311e4caf2
Cjt <jtongttt@gmail.com>
2021-02-03 22:06:36 +0800

    1.更新部分代码

--- project/PushLockScreenKtx/src/main/java/afkt/lock/ui/activity/MainActivity.kt
+++ project/PushLockScreenKtx/src/main/java/afkt/lock/ui/activity/MainActivity.kt
@@ -3,6 +3,8 @@ package afkt.lock.ui.activity
 import afkt.lock.R
 import afkt.lock.base.BaseActivity
 import afkt.lock.databinding.ActivityMainBinding
+import afkt.lock.receiver.DeviceReceiver
+import dev.utils.app.DevicePolicyUtils
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
@@ -10,5 +12,16 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun initListener() {
         super.initListener()
+
+        DevicePolicyUtils.getInstance()
+            .setComponentName(DeviceReceiver::class.java)
+
+        // 申请权限
+        binding.vidAmApplyBtn.setOnClickListener {
+            if (DevicePolicyUtils.getInstance().isAdminActive()) {
+                return@setOnClickListener
+            }
+            DevicePolicyUtils.getInstance().activeAdmin("需开启权限允许对设备进行操作!")
+        }
     }
 }
