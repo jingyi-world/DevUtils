fd5b210bcb1ba3be12452b551a134327aeba1637
Cjt <jtongttt@gmail.com>
2021-05-25 16:59:28 +0800

    1.更新部分 GeTuiPush 代码

--- module/push/DevGTPush/src/main/java/dev/gtpush/gtpush_convert.kt
+++ module/push/DevGTPush/src/main/java/dev/gtpush/gtpush_convert.kt
@@ -1,21 +1,48 @@
 package dev.gtpush
 
+import com.igexin.sdk.PushConsts
+import com.igexin.sdk.message.BindAliasCmdMessage
 import com.igexin.sdk.message.GTCmdMessage
 import com.igexin.sdk.message.GTNotificationMessage
 import com.igexin.sdk.message.GTTransmitMessage
 import dev.module.push.PushMessage
+import dev.utils.common.ConvertUtils
 
 fun convertGTCmdMessage(message: GTCmdMessage): PushMessage {
+    var code = ""
+    if (message.action == PushConsts.BIND_ALIAS_RESULT) {
+        if (message is BindAliasCmdMessage) {
+//            code 结果说明
+//            0：成功
+//            10099：SDK 未初始化成功
+//            30001：绑定别名失败，频率过快，两次调用的间隔需大于 5 s
+//            30002：绑定别名失败，参数错误
+//            30003：绑定别名请求被过滤
+//            30004：绑定别名失败，未知异常
+//            30005：绑定别名时，cid 未获取到
+//            30006：绑定别名时，发生网络错误
+//            30007：别名无效
+//            30008：sn 无效
+            code = message.code
+            val sn = message.sn
+        }
+    }
     return PushMessage(
+        code = code
     )
 }
 
 fun convertGTNotificationMessage(message: GTNotificationMessage): PushMessage {
     return PushMessage(
+        messageId = message.messageId,
+        title = message.title,
+        content = message.content,
     )
 }
 
 fun convertGTTransmitMessage(message: GTTransmitMessage): PushMessage {
     return PushMessage(
+        messageId = message.messageId,
+        extras = ConvertUtils.newString(message.payload),
     )
 }
