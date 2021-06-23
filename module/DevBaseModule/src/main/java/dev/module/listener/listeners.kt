ce14bf7a22e84187d5347536f8f3d952c50c673f
Cjt <jtongttt@gmail.com>
2021-06-23 12:16:57 +0800

    1.更新部分代码

---
+++ module/DevBaseModule/src/main/java/dev/module/listener/listeners.kt
@@ -0,0 +1,14 @@
+package dev.module.listener
+
+import dev.engine.share.listener.ShareListener
+import dev.module.share.ShareParams
+
+/**
+ * detail: 分享回调 封装接口
+ * @author Ttt
+ * <pre>
+ *     减少需要 ShareListener<ShareParams> 声明使用
+ *     内部直接封装具体泛型类
+ * </pre>
+ */
+interface DevShareListener : ShareListener<ShareParams>
