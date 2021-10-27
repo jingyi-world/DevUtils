16510cbaef4126e5176b99338a6a428f50f3b50c
Cjt <jtongttt@gmail.com>
2021-10-27 10:05:34 +0800

    1.更新部分代码

--- project/GTPushKtx/src/main/java/afkt/gtpush/push/gtpush_convert.kt
+++ project/GTPushKtx/src/main/java/afkt/gtpush/push/gtpush_convert.kt
@@ -38,16 +38,16 @@ private fun convertGTCmdMessage(message: GTCmdMessage): PushMessage {
     if (message.action == PushConsts.BIND_ALIAS_RESULT) {
         if (message is BindAliasCmdMessage) {
 //            code 结果说明
-//            0：成功
-//            10099：SDK 未初始化成功
-//            30001：绑定别名失败，频率过快，两次调用的间隔需大于 5 s
-//            30002：绑定别名失败，参数错误
-//            30003：绑定别名请求被过滤
-//            30004：绑定别名失败，未知异常
-//            30005：绑定别名时，cid 未获取到
-//            30006：绑定别名时，发生网络错误
-//            30007：别名无效
-//            30008：sn 无效
+//            0: 成功
+//            10099: SDK 未初始化成功
+//            30001: 绑定别名失败, 频率过快, 两次调用的间隔需大于 5 s
+//            30002: 绑定别名失败, 参数错误
+//            30003: 绑定别名请求被过滤
+//            30004: 绑定别名失败, 未知异常
+//            30005: 绑定别名时, cid 未获取到
+//            30006: 绑定别名时, 发生网络错误
+//            30007: 别名无效
+//            30008: sn 无效
             code = message.code
             val sn = message.sn
         }
