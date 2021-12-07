155e2a58487f15aac234a87c115dee79b6277386
Cjt <jtongttt@gmail.com>
2021-12-07 14:06:47 +0800

    1.更新 DevFinal 常量使用代码

--- interesting/DevStandard/src/main/java/dev/standard/sort/StateMain.java
+++ interesting/DevStandard/src/main/java/dev/standard/sort/StateMain.java
@@ -16,9 +16,17 @@ import dev.utils.common.StringUtils;
 /**
  * detail: State 类创建
  * @author Ttt
+ * <pre>
+ *     专门用于根据 {@link DevFinal.INT} 常量
+ *     生成 DevAssist 库中, dev.base.state 包下
+ *     CommonState、RequestState 类
+ * </pre>
  */
 public class StateMain {
 
+    // 请求字符串前缀
+    private static final String REQUEST_PREFIX  = "REQUEST_";
+    private static final String REQUEST_PREFIX2 = "Request_";
     // 开头字符串
     private static final String START_GET_STR;
     private static final String START_SET_STR;
@@ -62,6 +70,7 @@ public class StateMain {
     }
 
     public static void main(String[] args) {
+        boolean hasRequestFinal = false;
         // 循环所有常量
         Map<String, String> maps   = new HashMap<>();
         List<String>        lists  = new ArrayList<>();
@@ -69,12 +78,19 @@ public class StateMain {
         for (Field field : fields) {
             // 属于 Int 类型才处理
             if (field.getType().toString().equals("int")) {
-                String name = StringUtils.upperFirstLetter(field.getName().toLowerCase());
+                String fieldName = field.getName();
+                if (!hasRequestFinal) {
+                    if (fieldName.startsWith(REQUEST_PREFIX)) {
+                        hasRequestFinal = true;
+                    }
+                }
+
+                String name = StringUtils.upperFirstLetter(fieldName.toLowerCase());
                 lists.add(name);
 
                 String descriptor = Modifier.toString(field.getModifiers());
                 descriptor = StringUtils.isEmpty(descriptor) ? "" : descriptor + " ";
-                maps.put(name, descriptor + "int " + field.getName());
+                maps.put(name, descriptor + "int " + fieldName);
             }
         }
         // 获取该文件路径
@@ -87,6 +103,18 @@ public class StateMain {
         // = 移除多余代码 =
         // ==============
 
+        // 因为 REQUEST_ 常量复用 INT 变量所以需要判断是否存在 REQUEST 常量
+        if (hasRequestFinal) {
+            Iterator<String> iterator = lists.iterator();
+            while (iterator.hasNext()) {
+                String text = iterator.next();
+                if (text.startsWith(REQUEST_PREFIX2)) {
+                    break;
+                }
+                iterator.remove();
+            }
+        }
+
         String           first           = lists.get(0);
         String           firstDescriptor = maps.get(first);
         Iterator<String> iterator        = textLists.iterator();
@@ -96,6 +124,10 @@ public class StateMain {
                 break;
             }
             iterator.remove();
+        }
+
+        if (hasRequestFinal && first.endsWith("normal")) {
+            textLists.add(0, "// 默认状态 ( 暂未进行操作 )");
         }
 
         // ==========
@@ -128,28 +160,32 @@ public class StateMain {
             for (int i = 0, len = textLists.size(); i < len; i++) {
                 String context = textLists.get(i);
                 if (context.contains(descriptor)) {
-                    annotation = StringUtils.clearSpaceTabLineTrim(
-                            textLists.get(i - 1)
-                    );
-                    annotation = StringUtils.clearStartsWith(annotation, "// ");
+                    if (i != 0) {
+                        annotation = StringUtils.clearSpaceTabLineTrim(
+                                textLists.get(i - 1)
+                        );
+                        annotation = StringUtils.clearStartsWith(annotation, "// ");
+                    }
                 }
             }
-            String finalName = StringUtils.underScoreCaseToCamelCase(name);
+            if (annotation != null) {
+                String finalName = StringUtils.underScoreCaseToCamelCase(name);
 
-            getPartBuilder
-                    .append(DevFinal.SYMBOL.NEW_LINE)
-                    .append(String.format(
-                            IS_METHOD_STR, annotation,
-                            finalName, name.toUpperCase()
-                    ));
+                getPartBuilder
+                        .append(DevFinal.SYMBOL.NEW_LINE)
+                        .append(String.format(
+                                IS_METHOD_STR, annotation,
+                                finalName, name.toUpperCase()
+                        ));
 
-            setPartBuilder
-                    .append(DevFinal.SYMBOL.NEW_LINE)
-                    .append(String.format(
-                            SET_METHOD_STR, annotation,
-                            className, className,
-                            finalName, name.toUpperCase()
-                    ));
+                setPartBuilder
+                        .append(DevFinal.SYMBOL.NEW_LINE)
+                        .append(String.format(
+                                SET_METHOD_STR, annotation,
+                                className, className,
+                                finalName, name.toUpperCase()
+                        ));
+            }
         }
         // 最终内容
         String content = getPartBuilder.append(setPartBuilder).toString();
