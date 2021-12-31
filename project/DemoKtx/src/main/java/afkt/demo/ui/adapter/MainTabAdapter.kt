10c176b337e7ff95c14cb26fabc6fa8fa220f1db
Cjt <jtongttt@gmail.com>
2021-12-31 09:41:24 +0800

    1.更新部分代码

--- project/DemoKtx/src/main/java/afkt/demo/ui/adapter/MainTabAdapter.kt
+++ project/DemoKtx/src/main/java/afkt/demo/ui/adapter/MainTabAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class MainTabAdapter(
-    val fragments: List<Fragment>,
+    private val fragments: List<Fragment>,
     activity: FragmentActivity
 ) : FragmentStateAdapter(activity) {
 
